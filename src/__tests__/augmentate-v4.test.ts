5c84bf186520c62291694ceb68d153a41ef6aa1e
Santi Miranda <santimir@protonmail.com>
2023-05-17 21:17:42 +0100

    test: improve tests

--- src/__tests__/augmentate-v4.test.ts
+++ src/__tests__/augmentate-v4.test.ts
@@ -4,12 +4,20 @@ import { join } from 'path';
 import { augmentateV4 } from '../augmentate-v4';
 
 describe('augmentateV4', () => {
-  const input = join(__dirname, './data/test');
-  const out = join(__dirname, './out');
-  it('should work', async () => {
+  it('should work with a single dir', async () => {
+    const input = join(__dirname, './data/test');
+    const out = join(__dirname, './out');
     await augmentateV4(input, out);
     const result = await readdir(join(out, 'test'));
     const inputFiles = await readdir(input);
     expect(result.length).toBe(inputFiles.length * 4 - 6);
+  });
+  it('should work with multiple dirs', async () => {
+    const input = join(__dirname, './data');
+    const out = join(__dirname, './outAll');
+
+    await augmentateV4(input, out);
+    const result = await readdir(out);
+    expect(result).toHaveLength(2);
   });
 });
