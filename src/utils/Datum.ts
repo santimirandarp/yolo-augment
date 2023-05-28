a3958a8f271cd78029e8e031d560146c33b2c457
Santi Miranda <santimir@protonmail.com>
2023-05-28 16:11:46 +0100

    feat: add original as case

--- src/utils/Datum.ts
+++ src/utils/Datum.ts
@@ -1,6 +1,6 @@
-import { Image } from 'image-js';
+import { Image } from "image-js";
 
-import { Augmentation } from '../types';
+import { Augmentation } from "../types";
 
 export class Datum {
   newImage: Image;
@@ -11,29 +11,29 @@ export class Datum {
     imageName: string,
     bbox: number[][],
     image: Image, //original image
-    augmentation: Augmentation,
+    augmentation: Augmentation
   ) {
     const fullHeight = image.height;
     const fullWidth = image.width;
     let result: { newImage: Image; newNumericLabels: string[] };
     switch (augmentation) {
-      case 'rac90':
+      case "rac90":
         result = {
           newImage: image.rotate(-90),
           newNumericLabels: bbox.map(([x1, y1, x2, y2, c]) =>
-            [y1, fullWidth - x2, y2, fullWidth - x1, c].join(','),
+            [y1, fullWidth - x2, y2, fullWidth - x1, c].join(",")
           ),
         };
         break;
-      case 'rc90':
+      case "rc90":
         result = {
           newImage: image.rotate(90),
           newNumericLabels: bbox.map(([x1, y1, x2, y2, c]) =>
-            [fullHeight - y2, x1, fullHeight - y1, x2, c].join(','),
+            [fullHeight - y2, x1, fullHeight - y1, x2, c].join(",")
           ),
         };
         break;
-      case 'r180':
+      case "r180":
         result = {
           newImage: image.rotate(180),
           newNumericLabels: bbox.map(([x1, y1, x2, y2, c]) =>
@@ -43,24 +43,30 @@ export class Datum {
               fullWidth - x1,
               fullHeight - y1,
               c,
-            ].join(','),
+            ].join(",")
           ),
         };
         break;
-      case 'blur':
+      case "blur":
         result = {
           newImage: image.blur({
             height: 3,
             width: 3,
           }),
-          newNumericLabels: bbox.map((datum) => datum.join(',')),
+          newNumericLabels: bbox.map((datum) => datum.join(",")),
+        };
+        break;
+      case "original":
+        result = {
+          newImage: image,
+          newNumericLabels: bbox.map((datum) => datum.join(",")),
         };
         break;
       default:
-        throw new Error('augmentation not supported');
+        throw new Error("augmentation not supported");
     }
     this.newImage = result.newImage;
-    this.newAnnotation = result.newNumericLabels.join(' ');
+    this.newAnnotation = result.newNumericLabels.join(" ");
     this.newImageName = `${augmentation}_${imageName}`;
   }
 }
