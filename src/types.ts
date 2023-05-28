1c55aa9e172e546cbd975309648ef03cf4e1a028
Santi Miranda <santimir@protonmail.com>
2023-05-28 16:14:14 +0100

    chore: other updates

--- src/types.ts
+++ src/types.ts
@@ -1,7 +1,27 @@
-export type Augmentation = 'rc90' | 'rac90' | 'r180' | 'blur';
+export type Augmentation = "rc90" | "rac90" | "r180" | "blur" | "original";
 export type AugmentOptions = {
+  /**
+   * @default `["rc90", "rac90", "r180"]`
+   */
   augmentations: Augmentation[];
-  /* chooses only one of the operations to transform the image */
+  /**
+   * Whether to output the original image as well as the augmented
+   * @default true
+   */
+  outOriginal: boolean;
+  /**
+   * @default "./augmentedData"
+   */
+  outDir: string;
+  /**
+   * chooses only one of the operations to transform the image
+   * @default false
+   */
+
   random: boolean;
-  className?: string;
+  /**
+   * @default ["faces"]
+   * Only used if the `_classes.txt` does not exist.
+   */
+  classNames: string[];
 };
