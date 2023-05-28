25e144ff4cad29b0a5b3eeb1dc5c7d0702ab62a6
Santi Miranda <santimir@protonmail.com>
2023-05-28 16:45:19 +0100

    chore: lint

--- src/__tests__/augmentV4.test.ts
+++ src/__tests__/augmentV4.test.ts
@@ -1,12 +1,12 @@
-import { readdir } from "node:fs/promises";
-import { join } from "path";
+import { readdir } from 'node:fs/promises';
+import { join } from 'path';
 
-import { augmentV4 } from "../augmentV4";
+import { augmentV4 } from '../augmentV4';
 
-describe("augmentV4", () => {
-  it("should work with a single dir", async () => {
-    const input = join(__dirname, "./data/test");
-    const out = join(__dirname, "./out/test");
+describe('augmentV4', () => {
+  it('should work with a single dir', async () => {
+    const input = join(__dirname, './data/test');
+    const out = join(__dirname, './out/test');
     await augmentV4(input, {
       outDir: out,
     });
@@ -14,15 +14,15 @@ describe("augmentV4", () => {
     const inputFiles = await readdir(input);
     expect(result.length).toBe(inputFiles.length * 4 - 6);
   });
-  it("should work with multiple dirs", async () => {
-    const input = join(__dirname, "./data");
-    const out = join(__dirname, "./outAll");
+  it('should work with multiple dirs', async () => {
+    const input = join(__dirname, './data');
+    const out = join(__dirname, './outAll');
 
     await augmentV4(input, {
-      augmentations: ["blur", "r180", "rc90"],
+      augmentations: ['blur', 'r180', 'rc90'],
       random: true,
       outDir: out,
-      classNames: ["dark logo", "old logo", "white logo"],
+      classNames: ['dark logo', 'old logo', 'white logo'],
     });
     const result = await readdir(out);
     expect(result).toHaveLength(3);
