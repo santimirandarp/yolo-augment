4d281a0df8adb911cfaaa49aabb0c61efe890451
Santi Miranda <santimir@protonmail.com>
2023-05-29 13:26:40 +0100

    fix: do not run all if random is true and tests

--- src/__tests__/augmentV4.test.ts
+++ src/__tests__/augmentV4.test.ts
@@ -1,32 +1,56 @@
-import { readdir } from 'node:fs/promises';
-import { join } from 'path';
+import { readdir } from "node:fs/promises";
+import { join } from "path";
 
-import { augmentV4 } from '../augmentV4';
+import { augmentV4 } from "../augmentV4";
 
-describe('augmentV4', () => {
-  it('should throw if no data in dir', async () => {
-    const input = join(__dirname, './data/empty');
+describe("augmentV4", () => {
+  it("throw if no data in dir", async () => {
+    const input = join(__dirname, "./data/empty");
     await expect(augmentV4(input)).rejects.toThrow();
   });
-  it('should work with a single dir', async () => {
-    const input = join(__dirname, './data/test');
-    const out = join(__dirname, './out/test');
+  it("parse data in a single dir", async () => {
+    const input = join(__dirname, "./data/test");
+    const out = join(__dirname, "./out/test");
     await augmentV4(input, {
       outDir: out,
+      augmentations: ["original", "rac90"],
     });
-    const result = await readdir(out);
+
+    // check number of images generated
+    const output = await readdir(out);
+    const outputImages = output.filter((file) => file.endsWith(".jpg"));
+    const outputText = output.filter((file) => file.endsWith(".txt"));
+
     const inputFiles = await readdir(input);
-    expect(result.length).toBe(inputFiles.length * 4 - 6);
+    const inputImages = inputFiles.filter((file) => file.endsWith(".jpg"));
+    const inputText = inputFiles.filter((file) => file.endsWith(".txt"));
+
+    expect(outputImages).toHaveLength(inputImages.length * 2);
+    expect(outputText).toHaveLength(inputText.length);
+    expect(inputFiles).toHaveLength(inputImages.length + inputText.length);
   });
-  it('should work with multiple dirs', async () => {
-    const input = join(__dirname, './data');
-    const out = join(__dirname, './outAll');
+  it("Using random the number of files must be the same", async () => {
+    const input = join(__dirname, "./data/test");
+    const out = join(__dirname, "./out/test2");
+    await augmentV4(input, {
+      outDir: out,
+      augmentations: ["original", "rac90", "r180", "rc90", "blur"],
+      random: true,
+    });
+    const output = await readdir(out);
+    const inputFiles = await readdir(input);
+
+    expect(output).toHaveLength(inputFiles.length);
+  });
+  it("Parse data in multiple dirs", async () => {
+    const input = join(__dirname, "./data");
+    const out = join(__dirname, "./outAll");
 
     await augmentV4(input, {
-      augmentations: ['blur', 'r180', 'rc90'],
+      augmentations: ["blur", "r180", "rc90"],
       random: true,
       outDir: out,
-      classNames: ['dark logo', 'old logo', 'white logo'],
+      classNames: ["dark logo", "old logo", "white logo"],
     });
     const result = await readdir(out);
     expect(result).toHaveLength(2);
--- src/augmentV4.ts
+++ src/augmentV4.ts
@@ -1,19 +1,19 @@
-import { type WriteStream } from 'node:fs';
-import { mkdir } from 'node:fs/promises';
-import { resolve, join, relative } from 'node:path';
+import { type WriteStream } from "node:fs";
+import { mkdir } from "node:fs/promises";
+import { resolve, join, relative } from "node:path";
 
-import { type Image, write as writeImage, read } from 'image-js';
+import { type Image, write as writeImage, read } from "image-js";
 
-import type { Augmentation, AugmentOptions } from './types';
+import type { Augmentation, AugmentOptions } from "./types";
 import {
   checkPathExist,
   getDataDirectories,
   Datum,
   parseYoloV4Annotation,
   writeClassesFile,
-} from './utils';
-import { getAugmentations } from './utils/getAugmentations';
-import { makeReadWriteStreams } from './utils/makeReadWriteStream';
+} from "./utils";
+import { getAugmentations } from "./utils/getAugmentations";
+import { makeReadWriteStreams } from "./utils/makeReadWriteStream";
 
 /**
  * Recursively finds `_annotations.txt` from `startSearchFrom` and outputs to {@link AugmentOptions["outDir"] }
@@ -23,13 +23,13 @@ import { makeReadWriteStreams } from './utils/makeReadWriteStream';
  */
 export async function augmentV4(
   startSearchFrom: string,
-  options: Partial<AugmentOptions> = {},
+  options: Partial<AugmentOptions> = {}
 ) {
   const {
-    augmentations = ['rac90', 'r180', 'rc90'],
-    outDir = './augmentedData',
+    augmentations = ["rac90", "r180", "rc90"],
+    outDir = "./augmentedData",
     random = false,
-    classNames = ['faces'],
+    classNames = ["faces"],
     outOriginal = true,
   } = options;
 
@@ -38,24 +38,20 @@ export async function augmentV4(
 
   await checkPathExist(baseDirectoryPath);
 
-  const deDuplicatedAugmentations = getAugmentations(
-    augmentations,
-    outOriginal,
-  );
-  const { newAugmentations } = deDuplicatedAugmentations;
+  const { newAugmentations } = getAugmentations(augmentations, outOriginal);
   const newAugmentationsLength = newAugmentations.length;
 
   const dataDirectories = await getDataDirectories(baseDirectoryPath);
   if (dataDirectories.length === 0) {
     throw new Error(
-      `No data directories found in ${baseDirectoryPath}. Please check that the directory is correct and that the data directories are named "_annotations.txt"`,
+      `No data directories found in ${baseDirectoryPath}. Please check that the directory is correct and that the data directories are named "_annotations.txt"`
     );
   }
 
   for (const annotationDirectory of dataDirectories) {
     const outputDirectory = join(
       baseOutputDirectory,
-      relative(baseDirectoryPath, annotationDirectory),
+      relative(baseDirectoryPath, annotationDirectory)
     );
     try {
       await checkPathExist(outputDirectory);
@@ -66,34 +62,22 @@ export async function augmentV4(
     // one line in, and `augmentations.length+1` lines out.
     const { readStream, writeStream } = makeReadWriteStreams(
       annotationDirectory,
-      outputDirectory,
+      outputDirectory
     );
 
     for await (const annotation of readStream) {
       const [imageName, bbox] = parseYoloV4Annotation(annotation);
       const image = await read(join(annotationDirectory, imageName));
-      if (random) {
-        const randomIndex = Math.floor(Math.random() * newAugmentationsLength);
-        await writeData(
-          outputDirectory,
-          imageName,
-          image,
-          newAugmentations[randomIndex],
-          bbox,
-          writeStream,
-        );
-      }
-
-      for (const augmentation of newAugmentations) {
-        await writeData(
-          outputDirectory,
-          imageName,
-          image,
-          augmentation,
-          bbox,
-          writeStream,
-        );
-      }
+      await writeData(
+        outputDirectory,
+        imageName,
+        image,
+        random
+          ? newAugmentations[randomIndex(newAugmentationsLength)]
+          : newAugmentations,
+        bbox,
+        writeStream
+      );
     }
 
     writeStream.close();
@@ -110,18 +94,26 @@ async function writeData(
   outputDirectory: string,
   imageName: string,
   image: Image,
-  augmentation: Augmentation,
+  augmentation: Augmentation | Augmentation[],
   bbox: number[][],
-  writeStream: WriteStream,
+  writeStream: WriteStream
 ) {
-  const { newImageName, newImage, newAnnotation } = new Datum(
-    imageName,
-    bbox,
-    image,
-    augmentation,
-  );
-  const completeAnnotation = `${newImageName} ${newAnnotation}\n`;
+  augmentation = Array.isArray(augmentation) ? augmentation : [augmentation];
 
-  await writeImage(join(outputDirectory, newImageName), newImage);
-  writeStream.write(`${completeAnnotation}`);
+  for (const augmentationType of augmentation) {
+    const { newImageName, newImage, newAnnotation } = new Datum(
+      imageName,
+      bbox,
+      image,
+      augmentationType
+    );
+    const completeAnnotation = `${newImageName} ${newAnnotation}\n`;
+
+    await writeImage(join(outputDirectory, newImageName), newImage);
+    writeStream.write(`${completeAnnotation}`);
+  }
+}
+
+function randomIndex(arrLength: number) {
+  return Math.floor(Math.random() * arrLength);
 }
